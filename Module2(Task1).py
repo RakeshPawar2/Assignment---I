49e689c84871936c320b262a9d19d52897f0fbb5
RakeshPawar2 <rakeshpawar5363@gmail.com>
2025-07-25 23:05:27 +0530

    task2 file

---
+++ Module2(Task1).py
@@ -0,0 +1,27 @@
+#Task 1: Perform Basic Mathematical Operations
+#1.  Takes two numbers as input from the user.
+A=int(input("Enetr the first Number: "))
+B=int(input("Enetr the second Number: "))
+
+#2.  Performs the basic mathematical operations on these two numbers:
+
+print("\nAddition: ",A+B)
+
+print("Subtraction: ", A-B)
+
+print("Multiplication: ",A*B)
+
+print("Division: ",A/B)
+
+print('''Enetr the first Number: 5
+Enetr the second Number: 10
+
+Addition:  15
+Subtraction:  -5
+Multiplication:  50
+Division:  0.5
+''')
+
+
+
+
---
+++ Module2(Task2).py
@@ -0,0 +1,7 @@
+#Task 2: Create a Personalized Greeting
+
+#1.Takes a user's first name and last name as input.
+first_Name=input("Enter your first name: ")
+Last_Name=input("Enter your Last name: ")
+
+print("\nHello",first_Name+Last_Name,"welcome to the python program")
